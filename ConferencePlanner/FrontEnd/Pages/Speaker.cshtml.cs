45eafa596fa71c4f7c206e3efef4d1a0b6735feb
Michael Marango <mike.marango@gmail.com>
2019-10-28 17:52:59 +0300

    Added speaker details

---
+++ ConferencePlanner/FrontEnd/Pages/Speaker.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ConferenceDTO;
+using FrontEnd.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FrontEnd.Pages
+{
+    public class SpeakerModel : PageModel
+    {
+        public SpeakerResponse Speaker { get; private set; }
+
+        public async Task<ActionResult> OnGet([FromServices]ApiClient api, int id)
+        {
+            Speaker = await api.GetSpeakerAsync(id);
+            if (Speaker == null) return NotFound();
+            return Page();
+        }
+    }
+}
