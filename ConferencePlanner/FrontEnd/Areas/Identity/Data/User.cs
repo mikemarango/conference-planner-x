4a54aa5f1a862e7caae851815851d338cabb3707
Michael Marango <mike.marango@gmail.com>
2019-10-28 19:07:50 +0300

    Added identity migrations

--- ConferencePlanner/FrontEnd/Areas/Identity/Data/User.cs
+++ ConferencePlanner/FrontEnd/Areas/Identity/Data/User.cs
@@ -9,5 +9,6 @@ namespace FrontEnd.Data
     // Add profile data for application users by adding properties to the User class
     public class User : IdentityUser
     {
+        public bool IsAdmin { get; set; }
     }
 }
