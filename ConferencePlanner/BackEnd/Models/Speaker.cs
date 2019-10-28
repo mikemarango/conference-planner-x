941f8dde58d2e65d424a872a014e23edc268e449
Michael Marango <mike.marango@gmail.com>
2019-10-28 13:40:34 +0300

    Create models in class library

--- ConferencePlanner/BackEnd/Models/Speaker.cs
+++ ConferencePlanner/BackEnd/Models/Speaker.cs
@@ -6,18 +6,7 @@ using System.Threading.Tasks;
 
 namespace BackEnd.Models
 {
-    public class Speaker
+    public class Speaker : ConferenceDTO.Speaker
     {
-        public int Id { get; set; }
-
-        [Required]
-        [StringLength(200)]
-        public string Name { get; set; }
-
-        [StringLength(4000)]
-        public string Bio { get; set; }
-
-        [StringLength(1000)]
-        public virtual string WebSite { get; set; }
     }
 }
