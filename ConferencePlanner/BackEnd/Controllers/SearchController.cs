d2f06a8857c3840559db68ceea4ff6eca13eb84f
Michael Marango <mike.marango@gmail.com>
2019-10-28 18:08:09 +0300

    Add search controller to api

---
+++ ConferencePlanner/BackEnd/Controllers/SearchController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BackEnd.Data;
+using BackEnd.Models;
+using ConferenceDTO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BackEnd.Controllers
+{
+    [Route("api/search")]
+    public class SearchController : Controller
+    {
+        private readonly ApplicationContext _context;
+
+        public SearchController(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<SearchResult>>> Search(SearchTerm term)
+        {
+            var query = term.Query;
+            var sessionResults = await _context.Sessions.Include(s => s.Track)
+                                                .Include(s => s.SessionSpeakers)
+                                                    .ThenInclude(ss => ss.Speaker)
+                                                .Where(s =>
+                                                    s.Title.Contains(query) ||
+                                                    s.Track.Name.Contains(query)
+                                                )
+                                                .ToListAsync();
+
+            var speakerResults = await _context.Speakers.Include(s => s.SessionSpeakers)
+                                                    .ThenInclude(ss => ss.Session)
+                                                .Where(s =>
+                                                    s.Name.Contains(query) ||
+                                                    s.Bio.Contains(query) ||
+                                                    s.WebSite.Contains(query)
+                                                )
+                                                .ToListAsync();
+
+            var results = sessionResults.Select(s => new SearchResult
+            {
+                Type = SearchResultType.Session,
+                Session = s.MapSessionResponse()
+            })
+            .Concat(speakerResults.Select(s => new SearchResult
+            {
+                Type = SearchResultType.Speaker,
+                Speaker = s.MapSpeakerResponse()
+            }));
+
+            return results.ToList();
+        }
+    }
+}
---
+++ ConferencePlanner/ConferenceDTO/SearchResult.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConferenceDTO
+{
+    public enum SearchResultType
+    {
+        Session,
+        Speaker
+    }
+    public class SearchResult
+    {
+        public SearchResultType Type { get; set; }
+        public SessionResponse Session { get; set; }
+        public SpeakerResponse Speaker { get; set; }
+    }
+}
