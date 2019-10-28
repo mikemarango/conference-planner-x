28a8fcd335f8f9ff39b86668f185ece073143312
Michael Marango <mike.marango@gmail.com>
2019-10-28 12:20:26 +0300

    The backend starter

---
+++ ConferencePlanner/BackEnd/Data/ApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using BackEnd.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackEnd.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(
+            DbContextOptions<ApplicationContext> options) 
+            : base(options)
+        { }
+
+        public DbSet<Speaker> Speakers { get; set; }
+    }
+}
