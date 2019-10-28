c2430b97b0c5788fc89b13c488284bc45dfa8bbf
Michael Marango <mike.marango@gmail.com>
2019-10-28 17:02:31 +0300

    Render sessions on index

--- ConferencePlanner/FrontEnd/Pages/Index.cshtml.cs
+++ ConferencePlanner/FrontEnd/Pages/Index.cshtml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ConferenceDTO;
+using FrontEnd.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
@@ -11,15 +13,43 @@ namespace FrontEnd.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        public bool IsAdmin { get; set; }
 
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
         }
+        public IEnumerable<IGrouping<DateTimeOffset?, SessionResponse>>
+            Sessions
+        { get; set; }
+        public IEnumerable<(int Offset, DayOfWeek? DayofWeek)>
+            DayOffsets
+        { get; set; }
+        public int CurrentDayOffset { get; set; }
 
-        public void OnGet()
+        public async Task OnGet([FromServices]ApiClient backendClient,
+                                int day = 0)
         {
+            //IsAdmin = User.IsAdmin();
 
+            CurrentDayOffset = day;
+
+            var sessions = await backendClient.GetSessionsAsync();
+
+            var startDate = sessions.Min(s => s.StartTime?.Date);
+
+            var offset = 0;
+            DayOffsets = sessions.Select(s => s.StartTime?.Date)
+                                 .Distinct()
+                                 .OrderBy(d => d)
+                                 .Select(day => (offset++, day?.DayOfWeek));
+
+            var filterDate = startDate?.AddDays(day);
+
+            Sessions = sessions.Where(s => s.StartTime?.Date == filterDate)
+                               .OrderBy(s => s.TrackId)
+                               .GroupBy(s => s.StartTime)
+                               .OrderBy(g => g.Key);
         }
     }
 }
